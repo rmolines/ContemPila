92cc748ec1df2653879c3c677618aa446c462faa
rmolines <rafaelmismael@gmail.com>
2018-09-03 10:23:13 -0300

    Implementa operador unário e parenteses, respostas em H2

--- H1/h1-comp.py
+++ H1/h1-comp.py
@@ -2,6 +2,7 @@ NUM = "NUM"
 EOF = "EOF"
 OP = "OP"
 OP2 = "OP2"
+PAR = "PAR"
 
 import re
 
@@ -14,28 +15,14 @@ class Analisador:
     def termo():
         resultado = 0
         tokenizador = Analisador.tokenizador
-        tokenizador.selecionarProximo()
+        
+        resultado += Analisador.analisarFator()        
         if (tokenizador.atual.tipo != EOF):
-            # print(tokenizador.atual.tipo, tokenizador.atual.valor)
-            if tokenizador.atual.tipo == NUM:
-                resultado += int(tokenizador.atual.valor)
-                tokenizador.selecionarProximo()
-                while (tokenizador.atual.tipo != OP and tokenizador.atual.tipo != EOF):
-                    if (tokenizador.atual.valor == "*"):
-                        tokenizador.selecionarProximo()
-                        if (tokenizador.atual.tipo == NUM):
-                            resultado *= int(tokenizador.atual.valor)
-                        else:
-                            raise ValueError ("Erro de sintaxe")
-                    elif (tokenizador.atual.valor == "/"):
-                        tokenizador.selecionarProximo()
-                        if tokenizador.atual.tipo == NUM:
-                            resultado /= int(tokenizador.atual.valor)
-                        else:
-                            raise ValueError ("Erro de sintaxe")
-                    tokenizador.selecionarProximo()
-            else:
-                raise ValueError ("Erro no primeiro token")
+            while ((tokenizador.atual.tipo != PAR and tokenizador.atual.tipo != OP) and tokenizador.atual.tipo != EOF):
+                if (tokenizador.atual.valor == "*"):
+                    resultado *= Analisador.analisarFator()
+                elif (tokenizador.atual.valor == "/"):
+                    resultado /= Analisador.analisarFator()
 
         return resultado
 
@@ -54,6 +41,34 @@ class Analisador:
 
         return resultado
 
+    def analisarFator():
+        resultado = 0
+        fator = 1
+        tokenizador = Analisador.tokenizador
+        tokenizador.selecionarProximo()
+        if (tokenizador.atual.tipo != EOF):
+            if tokenizador.atual.tipo == NUM:
+                resultado += int(tokenizador.atual.valor)
+                tokenizador.selecionarProximo()
+            elif tokenizador.atual.tipo == PAR:
+                # tokenizador.selecionarProximo()
+                resultado += Analisador.analisarExpressao()
+                # tokenizador.selecionarProximo()
+                if (tokenizador.atual.tipo != PAR):
+                    raise ValueError ("Não fechou parenteses")
+                else:
+                    tokenizador.selecionarProximo()
+            elif tokenizador.atual.tipo == OP:
+                if tokenizador.atual.valor == "-":
+                    fator *= -1
+                resultado += Analisador.analisarFator() * fator
+                # tokenizador.selecionarProximo()
+            else:
+                raise ValueError ("Token fator ou primeiro token inválido")
+
+        return resultado
+
+
 
 class Token:
     def __init__(self, tipo, valor):
@@ -64,8 +79,8 @@ class Tokenizador:
 
     def __init__(self, codigoFonte):
         tempCodigo = codigoFonte
-        while(re.search("\/\*((?!\/\*).)*\*\/", tempCodigo)):
-            tempCodigo = re.sub("\/\*((?!\/\*).)*\*\/", "", tempCodigo)
+        while(re.search("\/\*.*\*\/", tempCodigo)):
+            tempCodigo = re.sub("\/\*.*\*\/", "", tempCodigo)
         
         tempCodigo = re.sub("\/\*", "", tempCodigo)
         print(tempCodigo)
@@ -105,7 +120,12 @@ class Tokenizador:
                 valor = origem[self.posicao]
                 self.atual = Token(tipo, valor)
                 self.posicao += 1
-
+            
+            elif (origem[self.posicao] == "(" or origem[self.posicao] == ")"):
+                tipo = PAR
+                valor = origem[self.posicao]
+                self.atual = Token(tipo, valor)
+                self.posicao += 1
 
 Analisador.inicializarTokenizador(input("CODIGO: "))
 print(Analisador.analisarExpressao())
